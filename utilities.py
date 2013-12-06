3bca960b8b5272c134ac16c6e790bceb99c1db3a
Joe Jean <joeclef@hotmail.com>
2013-12-06 13:19:56 +0400

    enforce only student access policy

--- utilities.py
+++ utilities.py
@@ -7,9 +7,25 @@ mylist=[]
 
 for line in f:
     line2 = line.strip('\n')
-    mylist.append(line2)
+    line3 = line2.split(" ")
+    line4 = line3[1]+" "+line3[0]
+    print line4
+    mylist.append(line4)
 
 f.close()
 print mylist
 
 
+
+##filename= raw_input('Enter filename: ')
+##
+##f = open(filename, "r")
+##
+##mylist=[]
+##
+##for line in f:
+##    line2 = line.strip('\n')
+##    mylist.append(line2)
+##
+##f.close()
+##print mylist
